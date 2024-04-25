8edfe2dd131ebc72783e8915896e3963ef743c07
Julian <juli99nic@gmail.com>
2024-04-24 23:13:11 -0300

    Added home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'home-screen',
   standalone: true,
   imports: [
-    SidebarComponent
+    SidebarComponent,
+    CommonModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  public gamesList: Array<GameModel> = [
+    { name: 'Ahorcado', },
+    { name: 'Pregunta2', },
+    { name: 'Diablo II', },
+  ]
+}
 
+interface GameModel {
+  name: string
 }
