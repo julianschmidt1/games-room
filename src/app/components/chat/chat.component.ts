72d3f5a373bea82ceb43901e4336c29b0b96d5c6
Julian <juli99nic@gmail.com>
2024-05-11 16:35:32 -0300

    Functional chat

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,7 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, inject } from '@angular/core';
+import { AfterViewChecked, Component, ElementRef, OnInit, ViewChild, inject } from '@angular/core';
 import { Auth } from '@angular/fire/auth';
+import { Firestore, addDoc, collection, collectionData } from '@angular/fire/firestore';
 import { FormsModule } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { stringToColor } from '../../helpers/textToColor.helper';
+import { ToastService } from '../../services/toast.service';
 
 @Component({
   selector: 'chat-list',
@@ -13,48 +17,80 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit, AfterViewChecked {
+  @ViewChild('chatScroll', { static: false }) chatScroll: ElementRef | null = null;
+
   private _auth = inject(Auth);
+  private _firestore = inject(Firestore);
+  private _toastService = inject(ToastService);
   public visible: boolean = true;
   public currentUser: any;
   public newMessage: string = '';
+  private chatSubscription!: Subscription;
+  public messages: Array<MessageModel> = [];
+
 
   public ngOnInit(): void {
-    this.currentUser = this._auth.currentUser;
-    console.log(this.currentUser);
+    this._auth.onAuthStateChanged((loggedUser) => {
+      if (loggedUser) {
+        this.currentUser = loggedUser;
+      }
+    });
+
+    const messagesCollection = collection(this._firestore, 'chatMessages');
+    const obs = collectionData(messagesCollection);
+
+    this.chatSubscription = obs.subscribe({
+      next: (response) => {
+        const castedResponse = response as Array<any>
+        this.messages = castedResponse.sort((a, b) => a?.date.toDate().getTime() - b?.date.toDate().getTime());
+      },
+      error: (error) => {
+        this._toastService.errorMessage('Ocurrio un error al cargar los mensajes');
+        console.log(error);
+      }
+    })
+
   }
 
-  public messages: Array<MessageModel> = [
-    { message: 'Hola amigos como esytan maravilloso dia Hola amigos como esytan maravilloso diaHola amigos como esytan maravilloso dia', date: new Date().toString(), createdBy: 'julito' },
-    { message: 'Yo le dije milei se la come pero nadie me creia jajaja', date: new Date().toString(), createdBy: 'julito2' },
-    { message: '💀💀💀', date: new Date().toString(), createdBy: 'julito3' },
-    { message: 'JAJJAJAJ', date: new Date().toString(), createdBy: 'julito4' },
-  ];
+  ngAfterViewChecked(): void {
+    this.scrollToBottom();
+  }
+
+  get stringToColor() {
+    return stringToColor;
+  }
+
+  private scrollToBottom() {
+    if (this.chatScroll?.nativeElement) {
+      const { nativeElement } = this.chatScroll;
+      nativeElement.scrollTop = nativeElement.scrollHeight;
+    }
+  }
 
   public handleSubmitMessage(event: any, iconPress?: boolean) {
     const newMessage = this.newMessage.trim();
     if (!newMessage) return;
 
-    const chat = document.getElementById('chat-scroll');
     if (event.key === 'Enter' || iconPress) {
-      this.messages = [
-        ...this.messages,
-        {
-          message: newMessage,
-          date: new Date().toString(),
-          createdBy: this.currentUser?.email
-        }
-      ];
+      const messageObject: MessageModel = {
+        message: newMessage,
+        date: new Date(),
+        createdBy: this.currentUser?.email,
+        displayName: this.currentUser?.displayName
+      }
+
       this.newMessage = '';
-
-      console.log(this.messages)
-
+      const messagesCol = collection(this._firestore, 'chatMessages');
+      addDoc(messagesCol, messageObject);
+      this.scrollToBottom();
     }
   }
 }
 
 interface MessageModel {
   message: string,
-  date: string,
+  date: Date,
   createdBy: string,
+  displayName: string,
 } 
